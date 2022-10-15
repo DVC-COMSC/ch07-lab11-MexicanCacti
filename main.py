9448d8d0f153e050b02b8af9e8588d5612c9de98
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-15 00:33:08 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#numbers = [     [10, 11, 12, 13, 14],
-#               [20, 21, 22, 23, 24],
-#               [30, 31, 32, 33, 34]]
+numbers = [     [10, 11, 12, 13, 14],
+               [20, 21, 22, 23, 24],
+               [30, 31, 32, 33, 34]]
 
-numbers =[] 
-rnum = int(input('Enter the number of rows'))
-for i in range(rnum):
- row = list(map(int, input('Enter 5 values for a row: ').split()))
- numbers.append(row)
+#numbers =[] 
+#rnum = int(input('Enter the number of rows'))
+#for i in range(rnum):
+ #row = list(map(int, input('Enter 5 values for a row: ').split()))
+ #numbers.append(row)
 
 rnum = len(numbers)
 cnum = len(numbers[0])
